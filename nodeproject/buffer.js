5db5fc0fe1ee38216491b5430478ce8492840e3b
hatem <71508255+hatemfaysil16@users.noreply.github.com>
2022-08-30 22:01:41 +0200

    's'

--- nodeproject/buffer.js
+++ nodeproject/buffer.js
@@ -1,3 +1,12 @@
 buf = new Buffer(256);
 len = buf.write('Muahmmed Essa');
 console.log('octet:' + len);
+
+///////////////////////
+buf = new Buffer(26);
+for(var x=0; x<26;x++){
+    buf[x]=x+100;
+}
+console.log(buf.toString('utf8',0,6));
+console.log(buf.toString('ascil',0,6));
+console.log(buf.toString('ascil'));
